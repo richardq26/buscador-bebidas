80ba045f6a773ca4b064e2e246529e8ae27aa150
Aicos <richard.sw268@gmail.com>
2021-02-18 03:16:31 -0500

    creando header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header className="bg-primary">
+            <h1>Buscador de recetas de bebidas</h1>
+            
+            </header>
+    )
+}
